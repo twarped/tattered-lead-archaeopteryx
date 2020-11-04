42cf7138648988d92644e77983723d5757ebbbd6
Glitch (hello-express) <none>
2020-11-04 18:06:05 +0000

    🐡🚏 Checkpoint ./public/script.js:17055700/594 ./views/index.html:17055700/98

--- public/script.js
+++ public/script.js
@@ -1,3 +1,14 @@
 var ytdl = require("youtube-dl")
-document.querySelector("#download-button").addEventListener(()=)
-var video = ytdl()
+var fs = require("fs")
+var cors = require("cors")
+var url = document.querySelector("#yt-url").value
+var downloadbutton = document.querySelector("#download-button")
+downloadbutton.addEventListener("click",()=>{
+  var video = ytdl(url)
+  video.on('info',function(info){
+    console.log('Download started')
+    console.log('filename: ' + info._filename)
+    console.log('size: ' + info.size)
+  })
+  video.pipe(fs.createWriteStream('myvideo.mp4'))
+})
