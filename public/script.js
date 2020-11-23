601c6e3eb8da9b3b420ed9f58c8ef192b540d2a2
Glitch (hello-express) <none>
2020-11-23 15:35:07 +0000

    🤗🐊 Checkpoint ./public/script.js:17055700/41 ./views/index.html:17055700/125

--- public/script.js
+++ public/script.js
@@ -1,6 +1,6 @@
 var downloadbutton = document.getElementById("downloadbutton")
 downloadbutton.addEventListener("click",()=>{
   var url = document.getElementById("yturl").value
-  alert((url.value != "" ? url.value : "nope, fill out the thingy..."))
-  window.open(window.location+"download?url="+url)
+  alert((url != "" ? url : "nope, fill out the thingy..."))
+  if (url != "") window.open(window.location+"download?url="+url)
 })
