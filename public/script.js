177eb453c08135ef3d00d8835de22411a859cde9
Glitch (hello-express) <none>
2020-11-24 19:53:48 +0000

    🐞🌶 Checkpoint ./public/script.js:17055700/261

--- public/script.js
+++ public/script.js
@@ -1,6 +1,7 @@
 var downloadbutton = document.getElementById("downloadbutton")
 downloadbutton.addEventListener("click",()=>{
   var url = document.getElementById("yturl").value
-  alert((url != "" ? url : "nope, fill out the thingy..."))
-  if (url != "") window.open(window.location+"download?url="+url)
+  var urlchecker=/youtube\.com\/watch(\?|&)v=[a-zA-Z0-9]+/g
+  alert((url.match(urlchecker) ? url : "Type In a Correct Youtube URL"))
+  if (url.match(urlchecker)) window.open(window.location+"download?url="+url)
 })
