414a3cd71d9a2e8680ae4f414af15397a76a9a8a
Glitch (tattered-lead-archaeopteryx) <none>
2021-02-22 17:37:57 +0000

    👩👢 Checkpoint ./server.js:17055700/60

--- server.js
+++ server.js
@@ -17,7 +17,8 @@ app.get("/", (request, response) => {
 
 app.get("/watch", (req, res) => {
   //var video = youtubedl(url,{format:"mp4"})
-  youtubedl.getInfo(req.query.v, (err, info) => {
+  youtubedl.getInfo(req.query.v, [], function (err, info) {
+    //console.log(err)
     console.log(req.query.v)
     var url = req.query.v;
     //console.log(info);
