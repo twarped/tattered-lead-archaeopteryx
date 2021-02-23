1efa50f923bd5774ad9b428b8272e98fd1101028
Glitch (tattered-lead-archaeopteryx) <none>
2021-02-23 20:22:49 +0000

    🎿🏖 Checkpoint ./server.js:17055700/4652

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.get("/watch", (req, res) => {
   //var video = youtubedl(url,{format:"mp4"})
   youtubedl.getInfo(req.query.v, function(err, info) {
     //console.log(err)
-    console.log(req.query.v);
+    console.log("retrieved info!");
     var url = req.query.v;
     //console.log(info);
     var title = info.title;
@@ -28,13 +28,35 @@ app.get("/watch", (req, res) => {
     //res.header("Content-Disposition", `attachment; filename="${title}.mp4"`);
     const video = youtubedl(url);
     var stream = video.pipe(
-      fs
-        .createWriteStream(
-          "/rbd/pnpm-volume/46445d14-adc1-4847-b269-fdfb11fa2547/youtubevids/" +
-            title +
-            ".mp4"
-        )
+      fs.createWriteStream(
+        "/rbd/pnpm-volume/46445d14-adc1-4847-b269-fdfb11fa2547/youtubevids/" +
+          title +
+          ".mp4"
+      )
     );
+    stream.on("close", () => {
+      console.log("finished downloading!");
+      var vidPath = path.join(
+        "/rbd/pnpm-volume/46445d14-adc1-4847-b269-fdfb11fa2547/youtubevids/"+
+        title,
+        ".mp4"
+      );
+      var stat = fs.statSync(vidPath);
+      res.writeHead(200, {
+        "Content-Type": "video/mp4",
+        "Content-Length": stat.size
+      });
+      var vidStream = fs.createReadStream(vidPath);
+      vidStream.pipe(res);
+      fs.unlink(
+        "/rbd/pnpm-volume/46445d14-adc1-4847-b269-fdfb11fa2547/youtubevids/" +
+          title +
+          ".mp4",
+        err => {
+          if (err) throw err;
+        }
+      );
+    });
   });
 });
 
