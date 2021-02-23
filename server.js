1ddf1693e4b15b9eb040daf7159081fec06a54e7
Glitch (tattered-lead-archaeopteryx) <none>
2021-02-23 20:00:23 +0000

    🚟🚂 Checkpoint ./server.js:17055700/3590

--- server.js
+++ server.js
@@ -18,27 +18,44 @@ app.get("/", (request, response) => {
 
 app.get("/watch", (req, res) => {
   //var video = youtubedl(url,{format:"mp4"})
-  youtubedl.getInfo(req.query.v, async function (err, info) {
+  youtubedl.getInfo(req.query.v, async function(err, info) {
     //console.log(err)
-    console.log(req.query.v)
+    console.log(req.query.v);
     var url = req.query.v;
     //console.log(info);
     var title = info.title;
     //console.log(info.title);
     //res.header("Content-Disposition", `attachment; filename="${title}.mp4"`);
     const video = youtubedl(url);
-    await video.pipe(fs.createWriteStream("/rbd/pnpm-volume/46445d14-adc1-4847-b269-fdfb11fa2547/youtubevids/"+title+".mp4"));
-    var vidPath = path.join("/rbd/pnpm-volume/46445d14-adc1-4847-b269-fdfb11fa2547/youtubevids/",title,".mp4");
-    var stat = fs.statSync(vidPath);
-    res.writeHead(200, {
-        'Content-Type': 'audio/mpeg',
-        'Content-Length': stat.size
+    var stream = video.pipe(
+      fs.createWriteStream(
+        "/rbd/pnpm-volume/46445d14-adc1-4847-b269-fdfb11fa2547/youtubevids/" +
+          title +
+          ".mp4"
+      )
+    );
+    stream.on("finish", () => {
+      var vidPath = path.join(
+        "/rbd/pnpm-volume/46445d14-adc1-4847-b269-fdfb11fa2547/youtubevids/",
+        title,
+        ".mp4"
+      );
+      var stat = fs.statSync(vidPath);
+      res.writeHead(200, {
+        "Content-Type": "audio/mpeg",
+        "Content-Length": stat.size
+      });
+      var vidStream = fs.createReadStream(vidPath);
+      vidStream.pipe(res);
+      fs.unlink(
+        "/rbd/pnpm-volume/46445d14-adc1-4847-b269-fdfb11fa2547/youtubevids/" +
+          title +
+          ".mp4",
+        err => {
+          if (err) throw err;
+        }
+      );
     });
-    var vidStream = fs.createReadStream(vidPath);
-    vidStream.pipe(res);
-    fs.unlink("/rbd/pnpm-volume/46445d14-adc1-4847-b269-fdfb11fa2547/youtubevids/"+title+".mp4", (err) => {
-      if (err) throw err;
-    })
   });
 });
 
