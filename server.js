fcd9fbff8a60261892631c2912b32a49a111df7e
Glitch (tattered-lead-archaeopteryx) <none>
2021-02-26 01:58:56 +0000

    🛣🎥 Checkpoint ./server.js:17055700/663

--- server.js
+++ server.js
@@ -1,19 +1,10 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const ytdlcore = require("ytdl-core");
 const got = require("got");
 const youtubedl = require("youtube-dl");
-const fs = require("fs");
-const path = require("path");
-const ffmpeg = require("fluent-ffmpeg");
-const jwt = require("express-jwt");
-const cookieparser = require("cookie-parser");
-//const axios = require("axios");
-const fetch = require("node-fetch");
 const request = require("request");
 
-app.use(cookieparser());
 app.use(express.static("public"));
 app.use(cors());
 app.use(express.json());
@@ -33,9 +24,13 @@ app.get("/watch", (req, res) => {
 });
 
 app.get("/playlist", (req, res) => {
-  youtubedl.getInfo("https://www.youtube.com/playlist?list=PLLu_K5OA-nxzrrmOUB7_NZ2hbIX7qGvfr", function(err, info) {
+  const playlist = youtubedl("https://www.youtube.com/playlist?list=PLLu_K5OA-nxzrrmOUB7_NZ2hbIX7qGvfr");
+  playlist.on('info', (info) => {
     res.send(info)
-  });
+  })
+  playlist.on('error', (err) => {
+    res.send(err)
+  })
 });
 
 const listener = app.listen(process.env.PORT, () => {
