07fa805bed2deb896997200e930be5dcb067c956
Glitch (tattered-lead-archaeopteryx) <none>
2021-03-01 15:06:28 +0000

    🎆🚤 Checkpoint ./server.js:17055700/778

--- server.js
+++ server.js
@@ -11,16 +11,10 @@ const consolemirror = require("console-mirror")
 
 consolemirror({app, clientPath:"console"})
 
-//app.use("/console", require("console-mirror")())
 app.use(express.static("public"));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
-//const consolemirror = require('console-mirror')({ app, clientPath: '/console' });
-
-//require("console-mirror")({ app })
-//app.use("/logs", require("console-mirror")())
 
 app.get("/", (request, response) => {
   response.sendFile(__dirname + "/views/index.html");
@@ -39,34 +33,39 @@ app.get("/watch", (req, res) => {
 });
 
 app.get("/playlist", (req, res) => {
-  var playlist = youtubedl(
-    "https://www.youtube.com/playlist?list=PLLu_K5OA-nxzrrmOUB7_NZ2hbIX7qGvfr"
-  );
-  playlist.on("info", info => {
-    res.send("no error: " + info.split("} {").join(","));
-  });
-  playlist.on("error", async err => {
-    var zip = archiver("zip");
-    var data = err.stdout.split("\n");
-    data = JSON.parse("[" + data + "]");
-    res.header(
-      "Content-Disposition",
-      contentdisposition(data[0].playlist + ".zip")
-    );
-    console.log(data.length);
-    res.sendStatus(200)
-    //zip.pipe(res);
-    for (var i of data) {
-      console.log(i.title)
-      var title =
-        i.title.indexOf(".") === i.title.length - 1
-          ? i.title.substring(0, i.title.length - 1) + ".mp4"
-          : i.title + ".mp4";
-        if (i == 0) zip.pipe(res)
-        zip.append(request(i.url), { name: title + ".mp4" });
-    }
-    zip.finalize();
-  });
+  youtubedl.getInfo("https://www.youtube.com/playlist?list=PLLu_K5OA-nxzrrmOUB7_NZ2hbIX7qGvfr", (err, info)=>{
+    if (err) res.send(err); else res.send(info)
+  })
+  // var playlist = youtubedl(
+  //   "https://www.youtube.com/playlist?list=PLLu_K5OA-nxzrrmOUB7_NZ2hbIX7qGvfr"
+  // );
+  // playlist.on("info", info => {
+  //   res.send("no error: " + info.split("} {").join(","));
+  // });
+  // playlist.on("error", err => {
+  //   res.send(err)
+  //   var zip = archiver("zip");
+  //   var data = err.stdout.split("\n");
+  //   data = JSON.parse("[" + data + "]");
+  //   //console.log(data)
+  //   res.header(
+  //     "Content-Disposition",
+  //     contentdisposition(data[0].playlist + ".zip")
+  //   );
+  //   console.log(data.length);
+  //   res.sendStatus(200)
+  //   //zip.pipe(res);
+  //   for (var i of data) {
+  //     console.log(i.title)
+  //     var title =
+  //       i.title.indexOf(".") === i.title.length - 1
+  //         ? i.title.substring(0, i.title.length - 1) + ".mp4"
+  //         : i.title + ".mp4";
+  //       if (i == 0) zip.pipe(res)
+  //       zip.append(request(i.url), { name: title + ".mp4" });
+  //   }
+  //   zip.finalize();
+  // });
 });
 
 var listener = app.listen()
