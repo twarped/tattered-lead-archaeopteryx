4de5fae43450f926ece289784648e9e46a0c9288
Glitch (tattered-lead-archaeopteryx) <none>
2021-03-02 17:42:11 +0000

    💰🌕 Checkpoint ./server.js:17055700/531

--- server.js
+++ server.js
@@ -19,8 +19,8 @@ app.get("/", (request, response) => {
 });
 
 app.get("/watch", (req, res) => {
-  if
-  youtubedl.getInfo(req.query.v, function(err, info) {
+  youtubedl.getInfo((req.query.v.indexOf("youtube.com/watch?") === -1 ? "https://www.youtube.com/watch?v=" : "") + req.query.v, function(err, info) {
+    if (err) res.send(err)
     var title =
       info.title.indexOf(".") === info.title.length - 1
         ? info.title.substring(0, info.title.length - 1) + ".mp4"
