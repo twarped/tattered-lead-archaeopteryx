1e2be5a8f63999dbe5170a18feb5f5fa95019353
Glitch (tattered-lead-archaeopteryx) <none>
2021-03-03 17:25:07 +0000

    💆🌮 Checkpoint ./public/playlistscript.js:17055700/549 ./views/playlist.html:17055700/39

--- public/playlistscript.js
+++ public/playlistscript.js
@@ -9,9 +9,15 @@ playlistIdsRequest.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
       var playlistIds = JSON.parse(playlistIdsRequest.responseText.split(`var ytInitialData = `)[1].split(`;</script><link rel="alternate" media="handheld" href="https://m.youtube.com/playlist?list=`)[0]);
       var playlistTitle = playlistIds.metadata.playlistMetadataRenderer.title;
+    } else {
+      alert(this.responseText);
     }
 };
-if (queryParams.list.includes("youtu" && "http" && "?list="))
+var playlistURL;
+if (queryParams.list.includes("youtu" && "http" && "?list=" && "/playlist")) playlistURL = queryParams.list; 
+else if (queryParams.list.indexOf("PL") === 0) playlistURL = "https://www.youtube.com/playlist?list=" + queryParams.list; 
+else if (queryParams.list.includes("youtu" && "http" && "&list=" && "/watch")) playlistURL = "https://www.youtube.com/playlist?list=" + queryParams.list.split("&list=")[1]
+console.log("")
 playlistIdsRequest.open("get", )
 function loadPlaylist(playlistTitle, playlistIds) {
   
