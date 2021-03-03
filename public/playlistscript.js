c19afad441c1303e3f7a0bf19dfab51c6dd6ddb4
Glitch (tattered-lead-archaeopteryx) <none>
2021-03-03 18:13:19 +0000

    🏡🍱 Checkpoint ./public/script.js:17055700/70 ./public/playlistscript.js:17055700/165

--- public/playlistscript.js
+++ public/playlistscript.js
@@ -7,23 +7,28 @@ for (var i of query) {
   queryParams[o[0]] = o[1]
 }
 alert("list: "+queryParams.list)
-var playlistIdsRequest = new XMLHttpRequest();
-playlistIdsRequest.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      alert(this.responseText)
-      var playlistIds = JSON.parse(playlistIdsRequest.responseText.split(`var ytInitialData = `)[1].split(`;</script><link rel="alternate" media="handheld" href="https://m.youtube.com/playlist?list=`)[0]);
-      var playlistTitle = playlistIds.metadata.playlistMetadataRenderer.title;
-    } else if (this.readyState == 4 && this.status != 200) {
-      alert("error? : "+this.status)
-    }
-};
+// var playlistIdsRequest = new XMLHttpRequest();
+// playlistIdsRequest.onreadystatechange = function() {
+//     if (this.readyState == 4 && this.status == 200) {
+//       alert(this.responseText)
+//       var playlistIds = JSON.parse(playlistIdsRequest.responseText.split(`var ytInitialData = `)[1].split(`;</script><link rel="alternate" media="handheld" href="https://m.youtube.com/playlist?list=`)[0]);
+//       var playlistTitle = playlistIds.metadata.playlistMetadataRenderer.title;
+//     } else if (this.readyState == 4 && this.status != 200) {
+//       alert("error? : "+this.status)
+//     }
+// };
 var playlistURL;
 if (queryParams.list.includes("youtu" && "http" && "?list=" && "/playlist")) playlistURL = queryParams.list; 
 else if (queryParams.list.indexOf("PL") === 0) playlistURL = "https://www.youtube.com/playlist?list=" + queryParams.list; 
 else if (queryParams.list.includes("youtu" && "http" && "&list=" && "/watch")) playlistURL = "https://www.youtube.com/playlist?list=" + queryParams.list.split("&list=")[1]
+window.fetch(playlistURL).then((data) => {
+  alert(data)
+}).catch((err) => {
+  alert(err)
+})
 alert("playlistURL: "+playlistURL)
-playlistIdsRequest.open("get", playlistURL)
-playlistIdsRequest.send();
+//playlistIdsRequest.open("get", playlistURL)
+//playlistIdsRequest.send();
 function loadPlaylist(playlistTitle, playlistIds) {
   
 }
