37019798006f8283b147f9ad265f21b3f3691337
Glitch (tattered-lead-archaeopteryx) <none>
2021-03-03 17:02:43 +0000

    🏤🗽 Checkpoint ./public/playlistscript.js:17055700/248 ./views/playlist.html:17055700/102 ./public/playliststyle.css:17055700/160

--- public/playlistscript.js
+++ public/playlistscript.js
@@ -1,2 +1,9 @@
-var queryParams = window.location.search.split("?").join("").split("&");
-alert(queryParams)
+var query = window.location.search.split("?").join("").split("&");
+var queryParams = {}
+for (var i of query) {
+  var o = i.split("=")
+  queryParams[o[0]] = o[1]
+}
+//alert(JSON.stringify(queryParams))
+var playlistIdsRequest = new XMLHttpRequest();
+playlistIdsRequest.open("get", )
