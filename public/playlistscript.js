4fcbb52cb8af13491d18dc2a66453bfa1b0a99cd
Glitch (tattered-lead-archaeopteryx) <none>
2021-03-04 23:46:20 +0000

    🖼🐬 Checkpoint ./views/playlist.ejs:17055700/38 ./public/playliststyle.css:17055700/29 ./public/playlistscript.js:17055700/4219

--- public/playlistscript.js
+++ public/playlistscript.js
@@ -1,36 +1,12 @@
-var query = window.location.search.split("?");
-var queryFirst = query.shift();
-//alert(JSON.stringify(query))
-var queryParams = {}
-for (var i of query) {
-  var o = i.split("=")
-  queryParams[o[0]] = o[1]
-}
-//alert("list: "+queryParams.list)
-// var playlistIdsRequest = new XMLHttpRequest();
-// playlistIdsRequest.onreadystatechange = function() {
-//     if (this.readyState == 4 && this.status == 200) {
-//       alert(this.responseText)
-//       var playlistIds = JSON.parse(playlistIdsRequest.responseText.split(`var ytInitialData = `)[1].split(`;</script><link rel="alternate" media="handheld" href="https://m.youtube.com/playlist?list=`)[0]);
-//       var playlistTitle = playlistIds.metadata.playlistMetadataRenderer.title;
-//     } else if (this.readyState == 4 && this.status != 200) {
-//       alert("error? : "+this.status)
-//     }
-// };
-var playlistURL;
-if (queryParams.list.includes("youtu" && "http" && "?list=" && "/playlist")) playlistURL = "/playlisttest"+queryParams.list.split("/playlist")[1]; 
-else if (queryParams.list.indexOf("PL") === 0) playlistURL = "/playlisttest?list=" + queryParams.list; 
-else if (queryParams.list.includes("youtu" && "http" && "&list=" && "/watch")) playlistURL = "/playlisttest?list=" + queryParams.list.split("&list=")[1]
-window.fetch(playlistURL).then((res) => {
-  alert(JSON.stringify(res))
-}).catch((err) => {
-  alert(typeof err)
-  alert(err)
-  alert(JSON.stringify(err))
-})
-alert("playlistURL: "+playlistURL)
-//playlistIdsRequest.open("get", playlistURL)
-//playlistIdsRequest.send();
-function loadPlaylist(playlistTitle, playlistIds) {
-  
-}
+window.onload = () => {
+  var playlistContentContainer = document.body.getElementsByClassName(
+    "playlistContentContainer"
+  );
+  for (var i in playlistContentContainer) {
+    playlistContentContainer[i].addEventListener("click", () => {
+      if (playlistContentContainer[i].style.backgroundColor != "#42f578")
+        playlistContentContainer[i].style.backgroundColor = "#42f578";
+      else playlistContentContainer[i].style.backgroundColor = "#f54242";
+    });
+  }
+};
