0185cb7da662ea9f8ba2f11ac8afb94fe9210e7b
Glitch (tattered-lead-archaeopteryx) <none>
2021-03-05 03:18:04 +0000

    👐🐩 Checkpoint ./public/detecterrors.js:17055700/580

--- public/detecterrors.js
+++ public/detecterrors.js
@@ -3,19 +3,20 @@ window.onerror = function(msg, url, lineNo, columnNo, error) {
   xhr.onload = () => {
     var errorWindow = window.open("", "", "");
     errorWindow.document.write("<body></body>");
-    errorWindow.document.body.textContent =
-      "Message: " +
-      msg +
-      "<br>URL/Script: " +
-      url +
-      "<br>Line Number: " +
-      lineNo +
-      "<br>Column/Char Number: " +
-      columnNo +
-      "<br>Error: " +
-      error +
-      "(Beta) Line and text: " +
-      JSON.stringify(xhr.response);
+    errorWindow.document.write(
+      // "Message: " +
+      // msg +
+      // "<br>URL/Script: " +
+      // url +
+      // "<br>Line Number: " +
+      // lineNo +
+      // "<br>Column/Char Number: " +
+      // columnNo +
+      // "<br>Error: " +
+      // error +
+      // "<br>(Beta) Line and text: " +
+      xhr.response.replaceAll("<", "&lt;").replaceAll(">", "&gt;").split("\n")[lineNo - 1]
+    )
   };
   xhr.open("get", url);
   xhr.send();
