ea841aa5c369a398586a1a74a6ef7f55bb9864cf
Glitch (tattered-lead-archaeopteryx) <none>
2021-03-05 00:20:35 +0000

    🐀😎 Checkpoint ./public/playlistscript.js:17055700/507

--- public/playlistscript.js
+++ public/playlistscript.js
@@ -1,3 +1,7 @@
+window.onerror = function (msg, url, lineNo, columnNo, error) {
+  var errorWindow = window.open("","","")
+  errorWindow.document.write = msg + "<br>" + url + "<br>" + lineNo + "<br>" + columnNo + "<br>" + error
+}
 var playlistContent = document.getElementById("playlistContainer").children
 playlistContent.forEach(playlistContentContainer => {
   playlistContentContainer.addEventListener("click", () => {
