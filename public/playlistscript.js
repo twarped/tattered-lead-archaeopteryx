993705131f72a8e77f5c489a39e91023a7f95062
Glitch (tattered-lead-archaeopteryx) <none>
2021-03-05 00:31:43 +0000

    👩‍👩‍👦🍋 Checkpoint ./public/playlistscript.js:17055700/2843

--- public/playlistscript.js
+++ public/playlistscript.js
@@ -1,10 +1,26 @@
-window.onerror = function (msg, url, lineNo, columnNo, error) {
-  var errorWindow = window.open("","","")
-  errorWindow.document.write = msg + "<br>" + url + "<br>" + lineNo + "<br>" + columnNo + "<br>" + error
-}
-var playlistContent = document.getElementById("playlistContainer").children
-playlistContent.forEach(playlistContentContainer => {
+window.onerror = function(msg, url, lineNo, columnNo, error) {
+  var errorWindow = window.open("", "", "");
+  errorWindow.document.write(
+    "Message: " +
+      msg +
+      "<br>URL/Script: " +
+      url +
+      "<br>Line Number: " +
+      lineNo +
+      "<br>Column/Char Number: " +
+      columnNo +
+      "<br>Error: " +
+      error
+  );
+};
+var playlistContent = document.getElementById("playlistContainer").children;
+for (var playlistContentContainer of playlistContent) {
+  playlistContentContainer.style.backgroundColor = "#42f578";
   playlistContentContainer.addEventListener("click", () => {
-    alert(this.style.backgroundColor)
-  })
-})
+    //var playlistContentContainerInfo = window.open("", "", "");
+    //playlistContentContainerInfo.document.write(playlistContentContainer.style.backgroundColor)
+    if (playlistContentContainer.style.backgroundColor != "rgb(66, 245, 120)")
+      playlistContentContainer.style.backgroundColor = "rgb(66, 245, 120)";
+    else playlistContentContainer.style.backgroundColor = "rgb(245, 66, 66)";
+  });
+}
