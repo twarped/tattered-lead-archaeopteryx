f8ac3314e12c4eea59d4c4aae9651fba01a48256
Glitch (tattered-lead-archaeopteryx) <none>
2021-03-05 15:56:29 +0000

    🎑🔋 Checkpoint ./watch.json:17055700/261 ./public/playlistscript.js:17055700/288 ./views/playlist.ejs:17055700/96 ./server.js:17055700/4

--- public/playlistscript.js
+++ public/playlistscript.js
@@ -5,16 +5,17 @@ function windowOnLoad(contents) {
     //playlistContent[i].videoId = window.contents.contents[i].playlistVideoRenderer.videoId;
     //playlistContent[i].shouldDownload = true;
     playlistContent[i].style.backgroundColor = "#42f578";
-    playlistContent[i].onclick = function changeColors() {
+    playlistContent[i].onclick = function changeColors(e) {
+      //alert(e)
       if (!this.shouldDownload) this.shouldDownload = false;
-      alert(this.shouldDownload)
-      // if (this.style.backgroundColor != "rgb(66, 245, 120)") {
-      //   this.style.backgroundColor = "rgb(66, 245, 120)";
-      //   this.shouldDownload = true;
-      // } else {
-      //   this.style.backgroundColor = "rgb(245, 66, 66)";
-      //   this.shouldDownload = false;
-      // }
+      //alert(this.videoId)
+      if (this.style.backgroundColor != "rgb(66, 245, 120)") {
+        this.style.backgroundColor = "rgb(66, 245, 120)";
+        this.shouldDownload = true;
+      } else {
+        this.style.backgroundColor = "rgb(245, 66, 66)";
+        this.shouldDownload = false;
+      }
     };
   }
 }
