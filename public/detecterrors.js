605f16a26831ae495e60454d0e49dfb883ae9f85
Glitch (tattered-lead-archaeopteryx) <none>
2021-03-05 03:41:17 +0000

    🍳😀 Checkpoint ./public/detecterrors.js:17055700/1793

--- public/detecterrors.js
+++ public/detecterrors.js
@@ -1,22 +1,31 @@
 window.onerror = function(msg, url, lineNo, columnNo, error) {
   var xhr = new XMLHttpRequest();
   xhr.onload = () => {
-    var data = xhr.response.replaceAll("<", "&lt;").replaceAll(">", "&gt;").split("\n")[(lineNo - 1)].split("")
+    var data = xhr.response
+      .replaceAll("<", "&lt;")
+      .replaceAll(">", "&gt;")
+      .replaceAll("&#34;", "\"")
+      .replaceAll("&quot;")
+      .split("\n")
+      [lineNo - 1].split("");
+    data[columnNo - 1] = ".....start of bad stuff: " + data[columnNo - 1];
+    data = data.join("");
     var errorWindow = window.open("", "", "");
     errorWindow.document.write("<body></body>");
-    errorWindow.document.write(
+    errorWindow.document.body.textContent = //(
       "Message: " +
-      msg +
-      "<br>URL/Script: " +
-      url +
-      "<br>Line Number: " +
-      lineNo +
-      "<br>Column/Char Number: " +
-      columnNo +
-      "<br>Error: " +
-      error +
-      "<br>(Beta) Line and text: " +
-    )
+        msg +
+        "<br>URL/Script: " +
+        url +
+        "<br>Line Number: " +
+        lineNo +
+        "<br>Column/Char Number: " +
+        columnNo +
+        "<br>Error: " +
+        error +
+        "<br>(Beta) Line and text: " +
+        data
+    //);
   };
   xhr.open("get", url);
   xhr.send();
