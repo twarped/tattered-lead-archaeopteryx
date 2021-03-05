4f3f96fae1f252b4edc56768057f22f2ffb53339
Glitch (tattered-lead-archaeopteryx) <none>
2021-03-05 03:29:37 +0000

    🏑🏐 Checkpoint ./public/detecterrors.js:17055700/412 ./views/playlist.ejs:17055700/158

--- public/detecterrors.js
+++ public/detecterrors.js
@@ -1,21 +1,21 @@
 window.onerror = function(msg, url, lineNo, columnNo, error) {
   var xhr = new XMLHttpRequest();
   xhr.onload = () => {
+    var data = xhr.response.replaceAll("<", "&lt;").replaceAll(">", "&gt;").split("\n")[(lineNo - 1)].split("")
     var errorWindow = window.open("", "", "");
     errorWindow.document.write("<body></body>");
     errorWindow.document.write(
-      // "Message: " +
-      // msg +
-      // "<br>URL/Script: " +
-      // url +
-      // "<br>Line Number: " +
-      // lineNo +
-      // "<br>Column/Char Number: " +
-      // columnNo +
-      // "<br>Error: " +
-      // error +
-      // "<br>(Beta) Line and text: " +
-      xhr.response.replaceAll("<", "&lt;").replaceAll(">", "&gt;").split("\n")[lineNo - 1]
+      "Message: " +
+      msg +
+      "<br>URL/Script: " +
+      url +
+      "<br>Line Number: " +
+      lineNo +
+      "<br>Column/Char Number: " +
+      columnNo +
+      "<br>Error: " +
+      error +
+      "<br>(Beta) Line and text: " +
     )
   };
   xhr.open("get", url);
