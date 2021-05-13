3d18021cef132e7859362a2a145f8a90111a8cf8
Glitch (tattered-lead-archaeopteryx) <none>
2021-05-13 17:11:32 +0000

    💿⛲️ Checkpoint ./server.js:17055700/116

--- server.js
+++ server.js
@@ -322,7 +322,7 @@ app.get("/waitstuffs", async (req, res) => {
       }
       var unblockSources = document.createElement("meta");
       unblockSources.httpEquiv = "content-security-policy";
-      unblockSources.content = `default-src 'unsafe-inline' ${htmlBaseLink}*; script-src 'unsafe-inline' ${htmlBaseLink}*; script-src-elem 'unsafe-inline' ${htmlBaseLink}*; style-src 'unsafe-inline' 'self' ${htmlBaseLink}*; style-src-elem 'self' 'unsafe-inline' ${htmlBaseLink}*;`;
+      unblockSources.content = `default-src 'unsafe-inline' https:; script-src 'unsafe-inline' https:; script-src-elem 'unsafe-inline' https:; style-src 'unsafe-inline' 'self' https:; style-src-elem 'self' 'unsafe-inline' https:;`;
       await document.head.appendChild(unblockSources);
       var styles = document.querySelectorAll("link[rel*='stylesheet']");
       for (var style of styles) {
