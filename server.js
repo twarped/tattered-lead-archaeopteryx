13b6fd73085ced496bce3817bce9fd439e50b719
Glitch (tattered-lead-archaeopteryx) <none>
2022-11-10 23:14:09 +0000

    🔨🌎 Checkpoint ./server.js:17055700/40582

--- server.js
+++ server.js
@@ -92,20 +92,18 @@ app.get("/watch", async (req, res) => {
         res.redirect(302, url); //iboss blocks proxy piping, so i just have to redirect you...
       } else {
         res.header("content-type", audio ? "audio/mpeg" : "video/mp4");
-        res.header(
-          "content-disposition",
-          contentdisposition(title + ext)
-        );
+        res.header("content-disposition", contentdisposition(title + ext));
         console.log(url);
-        console.log(title);
-        console.log(audio);
-        console.log(inbrowser);
-        console.log(ext);
-        console.log(res.getHeaders());
-        https.get(url, body => {
-          body.pipe(res);
-          res.end();
-        });
+        //console.log(title);
+        //console.log(audio);
+        //console.log(inbrowser);
+        //console.log(ext);
+        //console.log(res.getHeaders());
+        request(url, (err, rres, body) => {
+          
+        })
+        var proc = new ffmpeg({ source: request(url).on("response", rres => console.log(rres)) });
+        proc.withAudioCodec("libmp3lame").toFormat("mp3").output(res).run();
       }
     });
 });
